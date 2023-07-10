d54bcc8542849af386c04a25433bb5665e248ca8
Johan <johan.francocampos@gmail.com>
2023-07-10 11:00:20 -0400

    made into function

--- Sheet3/Draw_Face.py
+++ Sheet3/Draw_Face.py
@@ -1,13 +1,5 @@
 from gasp import *
 
-def moan():
-    print("Python is useless \nAnd so are these worksheets")
-
-def mult(x):
-    result = x*x
-    print("%d * %d is %d" %(x,x,result))
-
-
 # (300,250), 200 is what we used 
 #x =input("Insert x position of face")
 #y = input("Insert y position of face")
@@ -18,47 +10,52 @@ y = 250
 size = 200
 
 
+def face_maker(x , y, s):
+    face_cords = (int(x),int(y))
+    face_rad = int(size)
+    face_dam = int(size) * 2
 
-face_cords = (int(x),int(y))
-face_rad = int(size)
+    eyesize = face_rad/8
+    lefteyex  = face_cords[0] - (face_rad/2)
+    righteyex = face_cords[0] + (face_rad/2)
+    eye_ypos = face_cords[1] + (face_rad/4)
 
-eyesize = face_rad/8
-lefteyex  = face_cords[0] - (face_rad/2)
-righteyex = face_cords[0] + (face_rad/2)
-eye_ypos = face_cords[1] + (face_rad/4)
+    print(eye_ypos)
 
-print(eye_ypos)
+    x_nose_end = eyesize + lefteyex 
+    y_bottom_nose = (face_cords[1] - face_rad) + (face_rad*(3/4))
 
-x_nose_end = eyesize + lefteyex 
-y_bottom_nose = (face_cords[1] - face_rad) + (face_rad*(3/4))
+    arc_ypos = face_cords[1]*6/5
+    arc_rad  = face_rad
 
-arc_ypos = face_cords[1]*6/5
-arc_rad  = face_rad
-begin_graphics()
+    ear_y = (size * 5/4) + (face_cords[1]-face_rad)
+    ears = size *1/4
 
-Circle(face_cords, face_rad)
+    brow_y = face_cords[1]*4/5 
+    brow_s = size *3/4
 
-#Eyes
-Circle((lefteyex, eye_ypos), eyesize)
-Circle((righteyex,eye_ypos),eyesize)
+    begin_graphics()
 
-#Nose
-Line((face_cords[0],eye_ypos), (x_nose_end + (1/10 * face_rad), y_bottom_nose))
-Line(( x_nose_end + (1/10 * face_rad), y_bottom_nose), ((righteyex - eyesize - (1/10 *face_rad)), y_bottom_nose))
+    Circle(face_cords, face_rad)
 
-#Mouth
-Arc((face_cords[0], arc_ypos), arc_rad, 230, 310)
+    #Eyes
+    Circle((lefteyex, eye_ypos), eyesize)
+    Circle((righteyex,eye_ypos),eyesize)
 
-#Eyebrows
-Arc((lefteyex,200), 150, 110,70)
-Arc((righteyex,200), 150, 110,70)
+    #Nose
+    Line((face_cords[0],eye_ypos), (x_nose_end + (1/10 * face_rad), y_bottom_nose))
+    Line(( x_nose_end + (1/10 * face_rad), y_bottom_nose), ((righteyex - eyesize - (1/10 *face_rad)), y_bottom_nose))
 
-#Ears
-Arc((face_cords[0]+size,300),50, -90,120)
-Arc((face_cords[0]-size,300),50, 270,60)
-update_when('key_pressed')     
-end_graphics()
+    #Mouth
+    Arc((face_cords[0], arc_ypos), arc_rad, 230, 310)
 
-moan()
+    #Eyebrows
+    Arc((lefteyex, brow_y), brow_s, 110,70)
+    Arc((righteyex,brow_y), brow_s, 110,70)
 
-mult(3)
+    #Ears
+    Arc((face_cords[0]+size,ear_y),ears, -90,120)
+    Arc((face_cords[0]-size,ear_y),ears, 270,60)
+    update_when('key_pressed')     
+    end_graphics()
+face_maker(x , y, size)
