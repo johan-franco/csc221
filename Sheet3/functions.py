e3e2c9feb9047b4064e4157c8993d3893c36127a
Johan <johan.francocampos@gmail.com>
2023-07-10 10:44:39 -0400

    Full body added , made into function, issues fixed

---
+++ Sheet3/functions.py
@@ -0,0 +1,13 @@
+def moan():
+    print("Python is useless \nAnd so are these worksheets")
+
+def mult(x):
+    result = x*x
+    print("%d * %d is %d" %(x,x,result))
+
+
+moan()
+
+x = int(input("Insert Number: "))
+
+mult(x)
