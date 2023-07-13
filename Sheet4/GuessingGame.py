05f6074f5892b8690a3429f09446d24e289325a1
Johan <johan.francocampos@gmail.com>
2023-07-12 20:20:39 -0400

    implemented 2 function one for 2 player

--- Sheet4/GuessingGame.py
+++ Sheet4/GuessingGame.py
@@ -1,6 +1,16 @@
 from random import *
 from gasp.utils import *
+import getpass
 
+def new_game():
+    new_game = read_yesorno("Would you like to play again? ")
+    if new_game == True:
+        print("Choosing new number")
+        randnum = randint(min_range, max_range)
+        number_guesses = 0
+    else:
+        print("OK. Bye!")
+        exit()
 def adjust_settings(x,y):
     changer = read_yesorno("Would you like to change any settings? ")
     if changer == True :
@@ -15,11 +25,28 @@ def adjust_settings(x,y):
     change_player = read_yesorno("Would you like to change the amount of players?")
     if change_player == True:
         p = read_number("How many players do you want to change it to?")
-    
 
-    x = read_number("What minimum range do you want to play the guessing game from? ")
-    y = read_number("What maximum range do you want to play the guessing game from? ")
+    
     return (x, y)
+
+def twoplayer(s, pinfo):
+    s = 0
+    pname = input("Enter player 1's name: ")
+    pname2 = input("Enter player 2's name: ")
+    player1_guesser = 0
+    player2_guesser = 0
+    p1_games = 0
+    p2_games = 0
+    playerlist = [pname, pname2]
+    pinfo = [[pname, player1_guesser, p1_games], [pname2, player2_guesser, p2_games]]
+    entered_num = -1000000000
+    while entered_num > max_range or entered_num < min_range:
+        entered_num = int(getpass.getpass("%s enter your secret number:" %playerlist[s]))
+        print(entered_num)
+    s = 1 - s
+    return s & pinfo & entered_num
+
+
 
 wins = 0
 total_games = 0
@@ -34,9 +61,18 @@ Note that enabling this in second player will make winner be chosen by whoever i
 if limit_guesses == True:
     limiter = read_number("What limit of guesses do you want? ")
 else:
-    limiter = 0
+    limiter = -1
 
-randnum = randint(min_range,max_range)
+twoplay = read_yesorno("Would you like to play with 2 players (1v1)")
+
+if twoplay == True:
+    swap  = 0
+    playerinfo = 0
+    secret_num = 0
+    twoplayer(swap, playerinfo, secret_num )
+    guessernam= playerinfo[swap][0]
+else:
+    randnum = randint(min_range,max_range)
 
 print("A number between %d and %d has been chosen." %(min_range, max_range))
 
@@ -44,25 +80,21 @@ while True:
     guess = read_number("Enter your guess: ")
     number_guesses += 1
 
-    if guess == randnum or number_guesses == limiter:
-        if guess == randnum:
+    if guess == randnum or guess == secret_num:
+        if twoplay == True:
+            print("It took you %d guesses to guess %s's secret number, your total average guesses is %d" %(number_guesses, playerinfo[swap][0], ))
+        else:
             ListNumberGuesses.append(number_guesses)
             total_games +=1
             wins +=1
             winrate = (wins/total_games)*100
             avguess = sum(ListNumberGuesses)/len(ListNumberGuesses)
             print("Congrats you guessed my number! \nIt took you %d guesses.\nIn total you have played %d times, your total average guesses to the correct answer is %d and your winrate is %d% " %(number_guesses, total_games,avguess, winrate ))
-        else:
-            total_games +=1
-            print("You took %d guesses and went over the limiter!" %limiter)
-        new_game = read_yesorno("Would you like to play again? ")
-        if new_game == True:
-            print("Choosing new number")
-            randnum = randint(min_range, max_range)
-            number_guesses = 0
-        else:
-            print("OK. Bye!")
-            break
+        new_game()
+    elif number_guesses == limiter:
+        total_games +=1
+        print("You took %d guesses and went over the limiter!" %limiter)
+        new_game()
 
     elif guess < randnum:
         print("My number is higher than %d" %guess)
