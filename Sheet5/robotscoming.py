1d47b794972fc70dba3515fdfbcbe56bbfc5dbba
Johan <johan.francocampos@gmail.com>
2023-07-17 10:13:37 -0400

    Co-authored-by: FunnyManAnar <FunnyManAnar@users.noreply.github.com>

---
+++ Sheet5/robotscoming.py
@@ -0,0 +1,24 @@
+from gasp import *
+from pygame import *
+
+def place_player():
+    print("Here I am!")
+
+def move_player():
+    print("I'm moving...")
+    update_when('key_pressed')
+
+begin_graphics()
+
+finished = False
+
+place_player()
+
+while not finished:
+    move_player()
+
+end_graphics()
+
+
+
+
